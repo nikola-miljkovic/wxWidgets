4e24be40f849be218ea3e907927fcaa18be8f30c
Nikola Miljkovic <milja1337@gmail.com>
2014-06-25 00:32:34 +0200

    Added new frame creating to WXCalls and added code to distinct application without any wxTopLevelWindow.

--- src/android/java/WXCalls.java
+++ src/android/java/WXCalls.java
@@ -1,16 +1,50 @@
 package org.wxwidgets;
 
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 
 public class WXCalls {
 	
-	public void addView(FrameActivity activity, View view, int id, int width, int height, int x, int y) {
-		//View a = new View();
-    	//activity.addView(view, id, width, height, x, y);
-    }
-	
 	public static void addView(int id, int parentId, int width, int height, int x, int y, String classStr) {
-		WXApp.m_viewList.get(parentId).add(new WXView(id, parentId, new WXPair(x, y), 
-				 new WXPair(width, height), classStr));
+		WXFrameActivity frame = WXApp.m_frameMap.get(parentId);
+		
+		if(frame == null) {
+			WXApp.m_viewList.get(parentId).add(new WXView(id, parentId, new WXPair(x, y), 
+				new WXPair(width, height), classStr));
+		}
+		else {
+			WXView view = new WXView(id, parentId, new WXPair(x, y), new WXPair(width, height), classStr);
+			WXApp.m_viewList.get(parentId).add(view);
+			frame.putView(view.createView(frame), parentId, width, height, x, y);
+		}
 	}
+	
+	//
+    //    New wxFrame/wxDialog window
+    //
+    public static void createWindow(final int id, final String title) {
+		try {
+			WXApp.m_viewList.put(id, new ArrayList<WXView>());
+			Log.d(title, title);
+			if(WXApp.MAIN_ACTIVITY_INITIATED) {
+				Intent intent = new Intent(WXApp.MAIN_ACTIVITY, WXFrameActivity.class);
+				intent.putExtra("WX_TITLE", title);
+				intent.putExtra("WX_ID", id);
+				WXApp.MAIN_ACTIVITY.startActivityForResult(intent, 0);
+			}
+			else {
+				WXApp.MAIN_ACTIVITY.create(id, title);
+				WXApp.MAIN_ACTIVITY_INITIATED = true;
+			}
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+    }
+    
+    public static void invokeMethod() {
+    	
+    }
 }
