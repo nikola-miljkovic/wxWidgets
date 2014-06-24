33d91d9446f45f26b8a36d3e7f57b4169023c491
Nikola Miljkovic <milja1337@gmail.com>
2014-06-25 00:20:39 +0200

    Moved makeParams to WXApp, removed unneeded setters/getters, made containers variables public.

--- src/android/java/WXApp.java
+++ src/android/java/WXApp.java
@@ -2,64 +2,39 @@ package org.wxwidgets;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.Queue;
 
 import android.app.Application;
-import android.util.Log;
-import android.util.Pair;
-import android.view.View;
+import android.widget.RelativeLayout;
 
 public class WXApp extends Application {
-	
+	//public static Queue<Runnable> m_actionQueue = new LinkedList<Runnable>();
 	public static HashMap<Integer, ArrayList<WXView>> m_viewList = new HashMap<Integer, ArrayList<WXView>>();
+	public static HashMap<Integer, WXFrameActivity> m_frameMap = new HashMap<Integer, WXFrameActivity>();
 	
-	public static HashMap<Integer, List<Runnable>> m_frameActions  = new HashMap<Integer, List<Runnable>>();
-	private static int m_loadingCount = 0;
+	public static WXFrameActivity MAIN_ACTIVITY = null;
+	public static boolean MAIN_ACTIVITY_INITIATED = false;
 	
-	public boolean load(boolean toLoad) {
-		m_loadingCount += toLoad ? 1 : -1;
-		return m_loadingCount > 0;
-	}
-	
-	// ONLY NON NATIVE
-	private static HashMap<Integer, View> m_refMap = new HashMap<Integer, View>();
-	private static HashMap<Integer, FrameActivity> m_frameMap = new HashMap<Integer, FrameActivity>();
-	
-	public static MainActivity MAIN_ACTIVITY = null;
-	
-	public static void insertFrame(int id, FrameActivity frame) {
-		m_frameMap.put(id, frame);
-	}
-	
-	public static boolean removeFrame(int id) {
-		return m_frameMap.containsKey(id);
-	}
-	
-	public static void insertView(int id, View view) {
-		m_refMap.put(id, view);
-	}
-	
-	public static void wipeViews(FrameActivity parent) {
-		for(Entry<Integer, View> entry : m_refMap.entrySet()) {
-			if(entry.getValue().getContext() == parent) 
-				m_refMap.remove(entry.getKey());
-		}
-	}
-	
-	public static View getView(int id) {
-		return m_refMap.containsKey(id) ? m_refMap.get(id) : null;
-	}
-	
-	public static FrameActivity getFrame(int id) {
-		return m_frameMap.containsKey(id) ? m_frameMap.get(id) : null;
+	public static void wipeViews(int wxId) {
+		ArrayList<WXView> views = m_viewList.get(wxId);
+		for(WXView view : views) 
+			view.destroy();
 	}
 	
 	public static boolean canQuit() {
-		return m_refMap.isEmpty();
+		return m_frameMap.isEmpty();
 	}
 	
-	static void formatMe() {
-		Log.d("HI", "HIHIIHIH");
+	public static void executeAction(Runnable action) {
+		
 	}
+	
+	public static RelativeLayout.LayoutParams makeParams(int width, int height, int x, int y) {
+    	RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width, height);
+    	params.leftMargin = x; 
+    	params.topMargin = y;
+    	return params;
+    }
 }
