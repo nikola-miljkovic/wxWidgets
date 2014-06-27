e40a71172452e200681336aa4831a2d3304d925f
Nikola Miljkovic <milja1337@gmail.com>
2014-06-27 02:51:46 -0700

    Removed old code for android/private/wrappers.h and added new.
    New code calls WXCalls static methods, coded in java. This is required for matching new design of project.
    
    There is also WXSTR that will convert wxString to match jstring.

--- include/wx/android/private/wrappers.h
+++ include/wx/android/private/wrappers.h
@@ -10,54 +10,20 @@
 #ifndef _WX_ANDROID_WRAPPERS_H_
 #define _WX_ANDROID_WRAPPERS_H_
 
-#define INIT_ACTIVITY()
+#include "wx/android/private/globals.h"
 
-#define INIT_OBJ(_class, _args, ...) \
-	m_jclass = wxAndroid::Env->FindClass(_class); \
-	m_jobject = wxAndroid::Env->NewGlobalRef(wxAndroid::Env->NewObject(m_class, \
-		wxAndroid::Env->GetMethodID(m_class, "<init>", _args), __VA_ARGS__));
+inline jstring WXSTR(wxString str) 
+{
+	return wxAndroid::Env->NewStringUTF((const char*)str.mb_str(wxConvUTF8));
+}	
 
-#define FIND_CLASS(_class) \
-	m_jclass = wxAndroid::Env->FindClass(_class);
+#define CALL_STATIC_VOID(_method, _args, ...) \
+	wxAndroid::Env->CallStaticVoidMethod(wxAndroid::wxCalls, \
+		wxAndroid::Env->GetStaticMethodID(wxAndroid::wxCalls, _method, _args), __VA_ARGS__);
 
-#define CALL_VOID(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallVoidMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
+#define CALL_STATIC_INT(_method, _args, ...) \
+	wxAndroid::Env->CallStaticIntMethod(wxAndroid::wxCalls, \
+		wxAndroid::Env->GetStaticMethodID(wxAndroid::wxCalls, _method, _args), __VA_ARGS__);
 
-#define CALL_INT(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallIntMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_OBJ(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallObjectMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_BOOL(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallBooleanMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_BYTE(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallByteMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_CHAR(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallCharMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_SHORT(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallShortMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_LONG(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallLongMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_FLOAT(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallFloatMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
-
-#define CALL_DOUBLE(_class, _obj, _method, _args, ...) \
-	wxAndroid::Env->CallDoubleMethod(_obj, wxAndroid::Env->GetMethodID(_class, \
-		_method, _args), __VA_ARGS__);
 
 #endif // _WX_ANDROID_WRAPPERS_H_
