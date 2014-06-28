9826b08367a43935efc154f061cf646a10ee6c1d
Nikola Miljkovic <milja1337@gmail.com>
2014-06-28 15:38:23 -0700

    Added wxControl declarations and definitions.
    They are blank, just for wxButton demosntration.

--- include/wx/android/control.h
+++ include/wx/android/control.h
@@ -10,6 +10,8 @@
 #ifndef _WX_ANDROID_CONTROL_H_
 #define _WX_ANDROID_CONTROL_H_
 
+#if wxUSE_CONTROLS
+
 //-----------------------------------------------------------------------------
 // wxControl
 //-----------------------------------------------------------------------------
@@ -33,8 +35,24 @@ public:
             const wxValidator& validator = wxDefaultValidator,
             const wxString& name = wxControlNameStr);
 
+    virtual ~wxControl();
+    
+
+    virtual void Command(wxCommandEvent& event);
+
+    virtual void SetLabel(const wxString &label);
+    virtual wxString GetLabel() const;
+    virtual wxString GetLabelText() const;
+
+protected:
+    virtual wxSize DoGetBestSize() const;
+
 private:
+    wxString m_label;
+
     DECLARE_DYNAMIC_CLASS(wxControl)
 };
+
+#endif // wxUSE_CONTROLS
 
 #endif // _WX_ANDROID_CONTROL_H_
---
+++ src/android/jni/control.cpp
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        src/android/jni/control.cpp
+// Purpose:     
+// Author:      Nikola Miljkovic
+// Copyright:   (c) 2014 wxWidgets
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#include "wx/control.h"
+
+// ============================================================================
+// wxControl implementation
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// wxControl creation
+// ----------------------------------------------------------------------------
+
+IMPLEMENT_DYNAMIC_CLASS(wxControl, wxWindow)
+
+wxControl::wxControl()
+{
+}
+
+wxControl::~wxControl() 
+{
+
+}
+
+bool wxControl::Create( wxWindow *parent,
+                      wxWindowID id,
+                      const wxPoint &pos,
+                      const wxSize &size,
+                      long style,
+                      const wxValidator& validator,
+                      const wxString &name )
+{
+    return true;
+}
+
+void wxControl::Command(wxCommandEvent& event) 
+{
+
+}
+
+wxSize wxControl::DoGetBestSize() const
+{
+    return wxSize();
+}
+
+void wxControl::SetLabel(const wxString &label)
+{
+    m_label = label;
+}
+
+wxString wxControl::GetLabel() const
+{
+    return m_label;
+}
+
+wxString wxControl::GetLabelText() const
+{
+    return m_label;
+}
