92340d697b263e0db97f974748930795557c4e0a
Nikola Miljkovic <milja1337@gmail.com>
2014-06-28 15:19:44 -0700

    Adding new wrapper, fix to WXSTR(..).
    New macro for invokeViewMethod. It uses new MethodParams class(check next commit).
    WXSTR now takes reference instead of value.

--- include/wx/android/private/wrappers.h
+++ include/wx/android/private/wrappers.h
@@ -12,18 +12,23 @@
 
 #include "wx/android/private/globals.h"
 
-inline jstring WXSTR(wxString str) 
-{
-	return wxAndroid::Env->NewStringUTF((const char*)str.mb_str(wxConvUTF8));
-}	
+/////////////////////////////////////////////////////////////////////////////
+//
+//    Macros for calling WXCalls methods
+//
+/////////////////////////////////////////////////////////////////////////////	
 
 #define CALL_STATIC_VOID(_method, _args, ...) \
 	wxAndroid::Env->CallStaticVoidMethod(wxAndroid::wxCalls, \
 		wxAndroid::Env->GetStaticMethodID(wxAndroid::wxCalls, _method, _args), __VA_ARGS__);
 
-#define CALL_STATIC_INT(_method, _args, ...) \
-	wxAndroid::Env->CallStaticIntMethod(wxAndroid::wxCalls, \
-		wxAndroid::Env->GetStaticMethodID(wxAndroid::wxCalls, _method, _args), __VA_ARGS__);
+#define INVOKE_VIEW_METHOD(_method, ...) \
+	CALL_STATIC_VOID("invokeViewMethod", "(Ljava/lang/String;IIJ[Ljava/lang/Object;)V", wxAndroid::Env->NewStringUTF(_method), __VA_ARGS__);
 
+// Convert wxString to jstring
+inline jstring WXSTR(const wxString & str) 
+{
+	return wxAndroid::Env->NewStringUTF((const char*)str.mb_str(wxConvUTF8));
+}
 
 #endif // _WX_ANDROID_WRAPPERS_H_
