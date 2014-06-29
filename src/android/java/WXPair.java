66d3f044c08f23ba7224f3c71bde936aebc47216
Nikola Miljkovic <milja1337@gmail.com>
2014-06-29 16:15:09 -0700

    Added WXPair java class.
    This class is "structure" for holding two integer coordinates or size.

---
+++ src/android/java/WXPair.java
@@ -0,0 +1,15 @@
+package org.wxwidgets;
+
+public class WXPair { 
+	public int m_x, m_y; 
+	
+	public WXPair() {
+		m_x = -1;
+		m_y = -1;
+	}
+	
+	public WXPair(int x, int y) { 
+		m_x = x; 
+		m_y = y; 
+	} 
+} 
