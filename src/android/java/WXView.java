6879c96444e4eda6a08488dbc0974ee11a124d5f
Nikola Miljkovic <milja1337@gmail.com>
2014-07-09 12:09:33 -0700

    WXView will now hold pointer to corresponding view.
    This pointer value will be passed to event handlers in C++ in order to invoke them without searching for reference.

--- src/android/java/WXView.java
+++ src/android/java/WXView.java
@@ -4,8 +4,9 @@ import java.lang.reflect.Constructor;
 
 import android.content.Context;
 import android.view.View;
+import android.view.View.OnClickListener;
 
-public class WXView {
+public class WXView implements OnClickListener {
 	// id
 	public int m_id = 0;
 	public int m_parentId = 0;
@@ -15,8 +16,11 @@ public class WXView {
 	public String m_classStr;
 	public View m_view = null;
 	
-	public WXView(int id, int parentId, WXPair position, WXPair size, String classStr) {
+	private long m_ptr = 0;
+	
+	public WXView(int id, long ptr, int parentId, WXPair position, WXPair size, String classStr) {
 		m_id = id;
+		m_ptr = ptr;
 		m_parentId = parentId;
 		m_position = position;
 		m_size = size;
@@ -35,6 +39,9 @@ public class WXView {
 			Class viewClass = Class.forName(m_classStr);
 			Constructor constructor = viewClass.getConstructor(Context.class);
 			m_view = (View)constructor.newInstance(context);
+			synchronized(m_view) {
+				m_view.setOnClickListener(this);
+			}
 		} catch(Exception e)  {
 			e.printStackTrace();
 		}
@@ -49,5 +56,10 @@ public class WXView {
 	public void destroy() { 
 		// TO-DO remove view from layout here
 		m_view = null;
+	}
+
+	@Override
+	public void onClick(View v) {
+		WXNative.wxOnClickView(m_ptr);
 	}
 }
